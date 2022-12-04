29b5b467135687edddc97a69d4322203a12a58ef
Robson Mendonça <rob.ams@gmail.com>
2022-12-04 16:25:46 -0300

    fix: add docker p/ deploy.

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,11 +5,4 @@ run_cmd="dotnet run --no-build --urls http://0.0.0.0:5000 -v d"
 
 export PATH="$PATH:/root/.dotnet/tools"
 
-until dotnet ef database update; do
-    >&2 echo "Migrations executing"
-    sleep 1
-done
-
->&2 echo "DB Migrations complete, starting app."
->&2 echo "Running': $run_cmd"
 exec $run_cmd
